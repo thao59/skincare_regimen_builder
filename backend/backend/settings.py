8bd01edff9ce5ffc08058ada042e49f72fc841a4
Thao Dinh <dinh.thao0509@gmail.com>
2026-01-06 11:03:50 +1100

    changed email/password for smtp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,3 +151,10 @@ MEDIA_ROOT = BASE_DIR/ "media"
 load_dotenv()
 ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587 
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL")
+EMAIL_HOST_PASSWORD = os.getenv("PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL")
